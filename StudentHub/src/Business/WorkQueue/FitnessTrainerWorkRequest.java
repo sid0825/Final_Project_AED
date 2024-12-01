b1f8a12e6fb56eef22756245913e9df7a7ee39b4
O7580 <salian.o@northeastern.edu>
2024-12-01 18:12:51 -0500

    Added DormInventory and FitnessTrainerWorkRequest

---
+++ StudentHub/src/Business/WorkQueue/FitnessTrainerWorkRequest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author omkarsalian
+ */
+public class FitnessTrainerWorkRequest {
+    private String fitnessTrainerResult;
+
+    public String getFitnessTrainerResult() {
+        return fitnessTrainerResult;
+    }
+
+    public void setFitnessTrainerResult(String fitnessTrainerResult) {
+        this.fitnessTrainerResult = fitnessTrainerResult;
+    }
+}
