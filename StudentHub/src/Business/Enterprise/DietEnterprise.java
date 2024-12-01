f06056f740441c48c2065fa24e8fdca36777f452
O7580 <salian.o@northeastern.edu>
2024-12-01 01:13:08 -0500

    Implementted Diet and Doctor Enterprise

---
+++ StudentHub/src/Business/Enterprise/DietEnterprise.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+/**
+ *
+ * @author omkarsalian
+ */
+public class DietEnterprise extends Enterprise {
+
+    public DietEnterprise(String name) {
+        super(name, Enterprise.EnterpriseType.DietUnit);
+    }
+
+}
