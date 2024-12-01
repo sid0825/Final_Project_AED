d66202e9994288ee7c599bb00a0a494f3611a83c
Sumeet <rane.su@northeastern.edu>
2024-12-01 02:29:17 -0500

    Added role with getters and setters

--- StudentHub/src/Business/UserAccount/UserAccount.java
+++ StudentHub/src/Business/UserAccount/UserAccount.java
@@ -4,10 +4,51 @@
  */
 package Business.UserAccount;
 
+import Business.Role.Role;
+import Business.Student.Student;
+
 /**
  *
  * @author Sumeet
  */
 public class UserAccount {
+    
+    private String username;
+    private String password;
+    private Student student;
+    private Role role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
     
 }
