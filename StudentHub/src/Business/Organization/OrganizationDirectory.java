6be9687421d30cfbf5d5b692a4d57504b5284126
Sumeet <rane.su@northeastern.edu>
2024-12-01 13:14:36 -0500

    Added Organization class and OrganizationDirectory class

---
+++ StudentHub/src/Business/Organization/OrganizationDirectory.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sumeet
+ */
+public class OrganizationDirectory {
+    
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList<>();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+}
