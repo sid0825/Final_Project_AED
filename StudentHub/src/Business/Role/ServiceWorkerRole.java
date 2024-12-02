1aa21219acd57f774adeec16fafea494bb7b6636
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-12-01 20:27:31 -0500

    Implemented chnages for ui

--- StudentHub/src/Business/Role/ServiceWorkerRole.java
+++ StudentHub/src/Business/Role/ServiceWorkerRole.java
@@ -4,10 +4,23 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.ServiceWorkerRole.ServiceWorkerWorkAreaJPanel;
+
 /**
  *
  * @author siddheshsawant
  */
-public class ServiceWorkerRole {
+public class ServiceWorkerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new ServiceWorkerWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
     
 }
