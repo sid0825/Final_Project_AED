1c2a74332d730ddfd96f2c1dab80ade780fca69a
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-12-01 20:26:16 -0500

    Added code for Practitioner role

--- StudentHub/src/Business/Role/GeneralPractitionerRole.java
+++ StudentHub/src/Business/Role/GeneralPractitionerRole.java
@@ -4,10 +4,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.GeneralPractitionerRole.GenPractitionerWorkAreaJPanel;
+
 /**
  *
  * @author siddheshsawant
  */
-public class GeneralPractitionerRole {
-    
+public class GeneralPractitionerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new GenPractitionerWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
 }
