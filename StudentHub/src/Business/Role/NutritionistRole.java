d3939cbfcf676fe18855c407e045a3327664037f
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-12-01 20:26:34 -0500

    Implemented changes

--- StudentHub/src/Business/Role/NutritionistRole.java
+++ StudentHub/src/Business/Role/NutritionistRole.java
@@ -4,10 +4,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.NutritionistRole.NutritionWorkAreaJPanel;
+
 /**
  *
  * @author siddheshsawant
  */
-public class NutritionistRole {
-    
+public class NutritionistRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new NutritionWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
 }
