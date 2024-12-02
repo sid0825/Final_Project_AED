321294bdd28b62f903ae7cca7f36503893dda0d2
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-12-01 20:23:53 -0500

    Added contents in AdminRole

--- StudentHub/src/Business/Role/AdminRole.java
+++ StudentHub/src/Business/Role/AdminRole.java
@@ -4,10 +4,23 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import userinterface.AdministrativeRole.AdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author siddheshsawant
  */
-public class AdminRole {
+public class AdminRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business,Network network) {
+        return new AdminWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
     
 }
