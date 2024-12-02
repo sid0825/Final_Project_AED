798bd9654fe66656893a6ffa4940873fcfa69b18
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-12-01 20:28:22 -0500

    Implemented changes

--- StudentHub/src/Business/Role/SystemAdminRole.java
+++ StudentHub/src/Business/Role/SystemAdminRole.java
@@ -4,10 +4,22 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import userinterface.SystemAdminWorkArea.SystemAdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author siddheshsawant
  */
-public class SystemAdminRole {
+public class SystemAdminRole extends Role{
     
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, Enterprise enterprise, Organization organization, UserAccount account, EcoSystem business, Network network) {
+        return new SystemAdminWorkAreaJPanel(userProcessContainer, enterprise, organization, account, business, network);
+    }
 }
